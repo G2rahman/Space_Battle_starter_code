b5e203f8a77f2c167503f47cd647fcf4bbcf7e2e
G2rahman <94071678+G2rahman@users.noreply.github.com>
2021-12-22 20:35:43 -0500

    Add files via upload

---
+++ spacebattle.js
@@ -0,0 +1,26 @@
+let prompt = string("Do you want to attack or retreat?")
+let playerStatDisplay=document.querySelector(".playerhullhealth")
+let enemyStatDisplay=document.querySelector(".enemyStats")
+let endGameMessage =document.getElementById("endGameScreen")
+let gameContainer = document.querySelector(".bodyContainer") 
+let newGame=""
+let startMenuElements= document.querySelector("#startMenu")
+function revealGameElements(){
+    startMenuElements.style.display="none"
+    gameContainer.style.display="flex"
+
+    let document.getElementById.innerhtml=enemy1
+    let document.getElementById.innerhtml=enemy2
+    let document.getElementById.innerhtml=enemy3
+    let document.getElementById.innerhtml=enemy4
+    let document.getElementById.innerhtml=enemy5
+    let document.getElementById.innerhtml=enemy6
+
+}
+            }
+        
+       
+        
+        
+    }
+    
